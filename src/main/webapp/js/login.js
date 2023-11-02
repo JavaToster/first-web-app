5b773efad3e2350118abd43b08786537d8a2e61a
JavaToster <142619682+JavaToster@users.noreply.github.com>
2023-11-02 10:02:07 +0300

    version 2.1

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -6,8 +6,6 @@ function login(){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    document.cookie = 'username='+username
-
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/back", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -22,6 +20,7 @@ function login(){
         if (xhr.readyState === 4 && xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
             if(json.username == 'true' &&  json.password == 'true'){
+                document.cookie = 'username='+username;
                 window.open('http://localhost:8080/main', '_self');
             }else{
                 let label = document.getElementById('message');
